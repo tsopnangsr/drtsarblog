cd1d06fe8534d1ec8bf96a2ed3d34409f1f78e5a
Romaric Tsopnang <tsopnangsr@gmail.com>
2023-01-25 21:27:35 +0100

    Creating SEO-friendly links

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 app_name = 'blog'
 
-urlpattern = [
+urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+            views.post_detail,
+            name='post_detail'),
 ]
