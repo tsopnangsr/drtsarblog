ce270cceb15c6807e7f7b21c08c46e948e9ec5ed
Romaric Tsopnang <tsopnangsr@gmail.com>
2023-02-09 06:03:34 +0100

    Sending Email for recommendation

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def post_list(request):
@@ -48,7 +49,9 @@ def post_share(request, post_id):
     View (Function-based) to handle the recommandation from submission
     """
     # Retrieve post by id
-    post = get_object_or_404(Post, id=post_id, Status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
@@ -56,10 +59,29 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url} \n\n {cd['name']} \'s comments: {cd['comments']}"
+            #print(post_url)
+            #print(subject)
+            #print(message)
+            send_mail(subject, message, cd['email'], [cd['to']], fail_silently=False)
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 
                   'blog/post/share.html',
                   {'post': post,
-                   'form':form})
+                   'form': form,
+                   'sent': sent})
 # This is a comment for the documentation of this code
+
+'''
+send_mail(
+    'Django mail 2',
+    'Here is Djangor mail to test something.',
+    'tsopnangsr2@gmail.com',
+    ['contact@drtsar.com'],
+    fail_silently=False,
+)
+'''
--- drtsarblog/urls.py
+++ drtsarblog/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='home')),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
